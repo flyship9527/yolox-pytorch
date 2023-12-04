88d546ddbed0fa327266988b19831b6b22573d33
flyship <fyx0518@foxmail.com>
2023-12-04 11:32:28 +0800

    删除预训练

--- yolo.py
+++ yolo.py
@@ -25,7 +25,8 @@ class YOLO(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : '/content/yolox-pytorch/logs/best_epoch_weights.pth',
+        # "model_path"        : '/content/yolox-pytorch/logs/best_epoch_weights.pth',
+        "model_path": '',
         "classes_path"      : '/content/yolox-pytorch/model_data/voc_classes.txt',
         #---------------------------------------------------------------------#
         #   输入图片的大小，必须为32的倍数。
